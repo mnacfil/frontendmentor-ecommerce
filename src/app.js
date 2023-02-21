adb804c4faef423e2ad770d8c56d353251bde958
Melvin Nacfil <you@example.com>
2023-02-21 21:43:57 +0800

    add functionality in controllers and save the amount to local storage

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const productImages = [...document.querySelectorAll('.product-img')];
 const previousBtn = document.querySelector('.previous-btn');
 const nextBtn = document.querySelector('.next-btn');
-
+const addBtn = document.querySelector('.add-btn');
+const minusBtn = document.querySelector('.minus-btn');
+const amount = document.querySelector('.amount');
+const cartAmount = document.querySelector('.cart-amount');
 let currentIndex = 0;
 
 const checkIndex = (index) => {
@@ -29,9 +32,42 @@ const displayImage = (type) => {
     })
 }
 
+const addAmount = () => {
+    const currentAmount = Number(amount.textContent);
+    amount.textContent = currentAmount + 1;
+    updateCartAmount();
+    setAmountToLocalStorage();
+}
+const minusAmount = () => {
+    const currentAmount = Number(amount.textContent);
+    if(currentAmount === 0) return;
+    amount.textContent = currentAmount - 1;
+    updateCartAmount();
+    setAmountToLocalStorage();
+}
+
+const updateCartAmount = () => {
+    cartAmount.textContent = amount.textContent;
+}
+
+const setAmountToLocalStorage = () => {
+    localStorage.setItem('amount', amount.textContent);
+}
+
+const getAmountFromLocalStorage = () => {
+    const saveAmount = localStorage.getItem('amount');
+    if(!saveAmount) return 0;
+    return saveAmount;
+}
+
 nextBtn.addEventListener('click', () => {
     displayImage('next');
 });
 previousBtn.addEventListener('click', () => {
     displayImage('previous');
 });
+addBtn.addEventListener('click', addAmount);
+minusBtn.addEventListener('click', minusAmount);
+
+amount.textContent = getAmountFromLocalStorage();
+cartAmount.textContent = getAmountFromLocalStorage();
