a3b65d5d09945d8ff243cbba6f89b1a24862635b
Melvin Nacfil <you@example.com>
2023-02-22 15:53:42 +0800

    Setup empty state for cart

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const nextBtn = document.querySelector('.next-btn');
 const addBtn = document.querySelector('.add-btn');
 const minusBtn = document.querySelector('.minus-btn');
 const cartBtn = document.querySelector('.cart-btn');
+const deleteBtn = document.querySelector('.delete-btn');
 const amount = document.querySelector('.amount');
 const cartAmount = document.querySelector('.cart-amount');
 const cartModalNode = document.querySelector('.cart-modal');
@@ -11,7 +12,11 @@ const cartPrice = document.querySelector('.cart-price');
 const cartItems = document.querySelector('.items');
 const cartTotalPrice = document.querySelector('.total-price');
 const productPrice = document.querySelector('.product-price');
+const emptyCart = document.querySelector('.cart-empty');
+const cartContent = document.querySelector('.cart-content');
+
 let currentIndex = 0;
+// cartModalNode.savedHtml = cartModalNode.innerHTML;
 
 const checkIndex = (index) => {
     const imagesLength = productImages.length;
@@ -59,10 +64,18 @@ const updateCart = () => {
     cartItems.textContent = items || cartAmount.textContent;
     cartPrice.textContent = price || productPrice.textContent;
     cartTotalPrice.textContent = totalPrice || `$${Number(cartPrice.textContent.slice(1)) * Number(cartItems.textContent)}`;
+    setupCart();
 }
 
 const showCartModal = () => {
     cartModalNode.classList.toggle('show-cart-modal');
+}
+
+const removeCartProduct = () => {
+    removeCartfromLocalStorage();
+    setupCart();
+    amount.textContent = 0;
+    cartAmount.textContent = 0;
 }
 
 const setCartToLocalStorage = () => {
@@ -82,6 +95,26 @@ const getCartFromLocalStorage = () => {
     return cart;
 }
 
+const removeCartfromLocalStorage = () => {
+    localStorage.removeItem('cart');
+}
+
+const isCartEmpty = () => {
+    const cart = getCartFromLocalStorage();
+    if(Object.keys(cart).length === 0) return true;
+    return false;
+}
+
+const setupCart = () => {
+    if(isCartEmpty()) {
+        cartContent.classList.remove('show-cart-modal-content');
+        emptyCart.classList.add('show-cart-empty');
+    } else {
+        emptyCart.classList.remove('show-cart-empty');
+        cartContent.classList.add('show-cart-modal-content');
+    }
+}
+
 nextBtn.addEventListener('click', () => {
     displayImage('next');
 });
@@ -91,7 +124,7 @@ previousBtn.addEventListener('click', () => {
 addBtn.addEventListener('click', addAmount);
 minusBtn.addEventListener('click', minusAmount);
 cartBtn.addEventListener('click', showCartModal);
+deleteBtn.addEventListener('click', removeCartProduct);
 
-// amount.textContent = getAmountFromLocalStorage();
-// cartAmount.textContent = getAmountFromLocalStorage();
-updateCart()
+updateCart();
+setupCart()
